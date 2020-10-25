9fc21fed6622ebb092823e2028814bfc91096c3f
devatearth <64774809+devatearth@users.noreply.github.com>
2020-10-25 16:34:13 +0530

    fix for password validation while user creation

--- ImageHoster/src/main/java/ImageHoster/service/ValidatorService.java
+++ ImageHoster/src/main/java/ImageHoster/service/ValidatorService.java
@@ -3,6 +3,9 @@ package ImageHoster.service;
 import ImageHoster.model.User;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class ValidatorService {
 
@@ -13,5 +16,12 @@ public class ValidatorService {
        } else{
            return false;
        }
+    }
+    //Checks if the password contain 1 alphabet 1 number and 1 special charcter
+    public boolean checkPasswordStrength(String password){
+        String regex1 = "^(?=.*?[A-Za-z])(?=(.*[\\d]){1,})(?=(.*[\\W]){1,}).{0,}$";
+        Pattern p1 = Pattern.compile(regex1);
+        Matcher m1 = p1.matcher(password);
+        return (m1.matches() );
     }
 }
